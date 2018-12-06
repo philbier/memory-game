972bd4ab2410b744af15926f2cd55daf2c977768
PBI <phillip.biermann@nextlytics.com>
2018-12-06 09:49:11 +0100

    Initial Commit
    First commit to initialize the project

---
+++ js/app.js
@@ -0,0 +1,69 @@
+/* Main Controller that starts the game */
+(function mainController() {
+    
+    restart();
+
+})();
+
+function restart() {
+
+    //Create a list that holds all of your cards
+    const cardNodes = document.querySelectorAll(".card");
+    let cardsArray = Array.prototype.slice.call(cardNodes);
+    
+    //remove alle children
+    for (const element of cardNodes) {
+        element.remove();
+    };
+
+    //shuffle cardsArray
+    cardsArray = shuffle(cardsArray);
+    
+    //Add nodes to document
+    const deckNode = document.querySelector(".deck");
+    for (const element of cardsArray) {
+        deckNode.appendChild(element);
+    }
+
+}
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+
+
+// (function modelController() {
+
+// })();
+
+// (function uiController() {
+
+// })();
+
+
+
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
